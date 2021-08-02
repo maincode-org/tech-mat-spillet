5486af7a75c937461be305444bf80913f15a9691
MathiasHoeyrup <mathias.hoeyrup@gmail.com>
2021-08-02 09:04:08 +0200

    Removed unnecessary ionic CSS imports

--- src/screens/Game.tsx
+++ src/screens/Game.tsx
@@ -1,26 +1,7 @@
-import { IonContent, IonPage, IonSlides, IonSlide } from '@ionic/react'
-import functionPicture from '../assets/funktion.png'
-
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css'
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css'
-import '@ionic/react/css/structure.css'
-import '@ionic/react/css/typography.css'
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css'
-import '@ionic/react/css/float-elements.css'
-import '@ionic/react/css/text-alignment.css'
-import '@ionic/react/css/text-transformation.css'
-import '@ionic/react/css/flex-utils.css'
-import '@ionic/react/css/display.css'
-
-/* Theme variables */
-import '../styles/ionic-theme.css'
-import Theory from '../components/assignment/theory/Theory'
-import MathAssignment from '../components/assignment/math/MathAssignment'
+import { IonContent, IonPage, IonSlides, IonSlide } from '@ionic/react';
+import functionPicture from '../assets/funktion.png';
+import Theory from '../components/assignment/theory/Theory';
+import MathAssignment from '../components/assignment/math/MathAssignment';
 
 const Game: React.FC = () => {
   const slideOpts = {
--- src/screens/Progress.tsx
+++ src/screens/Progress.tsx
@@ -1,27 +1,5 @@
-import {
-    IonPage,
-    IonContent,
-} from '@ionic/react';
+import { IonPage, IonContent } from '@ionic/react';
 import DoughnutChart from "../components/chart/DoughnutChart";
-
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-/* Theme variables */
-import '../styles/ionic-theme.css';
 
 const Progress: React.FC = () => {
   return (
