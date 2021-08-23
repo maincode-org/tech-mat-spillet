305b8a0aab9371d8d7db6a48aebd0355a2cb7acc
Markido <markido@live.dk>
2021-08-23 12:13:16 +0200

    Moddet out API types

--- ionic-app/types/application-types.ts
+++ ionic-app/types/application-types.ts
@@ -1,11 +1,3 @@
-import { components } from './api-types';
-export type ITVShow = components['schemas']['Tv-show'];
-export type IEpisode = components['schemas']['Episode'];
-export type IRound = components['schemas']['Round'];
-export type IQuestion = components['schemas']['Question'];
-export type IFunFact = components['schemas']['Funfact'];
-export type IAnswerOption = components['schemas']['Question']['answerOptions'][0];
-
 export type IIcon = {
   id: string;
   name: string;
@@ -29,4 +21,3 @@ export type IIcon = {
 
 export type Override<T1, T2> = Omit<T1, keyof T2> & T2;
 
-export type IRoundData = Override<IRound, { icon: IIcon }>;
