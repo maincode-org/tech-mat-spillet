fee48e66083d75eb6eefa759cccf1911060a4fdc
Markido <markido@live.dk>
2021-08-24 21:55:17 +0200

    added several slides

--- ionic-app/src/screens/presentation/slides.tsx
+++ ionic-app/src/screens/presentation/slides.tsx
@@ -1,6 +1,10 @@
 import Welcome from '../../components/slides/welcome/Welcome';
 import XPlusTen from '../../components/slides/assignments/x-plus-ten/XPlusTen';
 import XPlusTenCode from '../../components/slides/assignments/x-plus-ten-code/XPlusTenCode';
+import WhoAreWe from '../../components/slides/who-are-we/WhoAreWe';
+import ContentPurpose from '../../components/slides/content-purpose/ContentPurpose';
+import OurBackground from '../../components/slides/our-background/OurBackground';
+import OurFocus from '../../components/slides/our-focus/OurFocus';
 
 type ISlide = {
   title?: string;
@@ -13,6 +17,25 @@ const slides: ISlide[] = [
     title: 'Vi præsenterer',
     subtitle: 'Matematik og programmering som spil',
     content: <Welcome />,
+  },
+  {
+    title: 'Hvem er vi?',
+    subtitle: 'Et nystartet indie udviklingsfirma',
+    content: <WhoAreWe />,
+  },
+  {
+    title: 'Vores baggrund',
+    content: <OurBackground />,
+  },
+  {
+    title: 'Vores fokus',
+    subtitle: 'User Experience - Gamification - Undervisningssituationer',
+    content: <OurFocus />,
+  },
+  {
+    title: 'Samspil mellem matematik og programmering',
+    subtitle: '\nIkke abstrakt, men praktisk og virkelighedsnært\n\n\nDe lærer på dansk, men koden kan auto-oversættes til Node.js',
+    content: <ContentPurpose />,
   },
   {
     content: <XPlusTen />,
