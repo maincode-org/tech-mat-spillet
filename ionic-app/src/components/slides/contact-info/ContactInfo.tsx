772d3201380ee840c931dac9d2bc7c1c842579b5
mathias <mathias.hoeyrup@gmail.com>
2021-08-25 19:15:25 +0200

    Fixed centering

--- ionic-app/src/components/slides/contact-info/ContactInfo.tsx
+++ ionic-app/src/components/slides/contact-info/ContactInfo.tsx
@@ -1,5 +1,5 @@
 import styles from './contact-info.module.css';
-import { IonImg } from '@ionic/react';
+import { IonImg, IonGrid, IonRow, IonCol } from '@ionic/react';
 import maincodeRobot from '../../../assets/maincode-robot.png';
 
 type IProps = {
@@ -9,17 +9,29 @@ type IProps = {
 const ContactInfo: React.FC<IProps> = ({ className }) => {
   return (
     <div className={`${className} w-full h-90 flex flex-row justify-around items-center`}>
-      <div className="flex flex-col justify-center items-center">
-        <h3>Mail</h3>
-        <p>mhn@maincode.dk</p>
-        <p>mark@maincode.dk</p>
-      </div>
-      <IonImg className={styles.img} src={maincodeRobot} />
-      <div className="flex flex-col justify-center items-center">
-        <h3>Telefon</h3>
-        <p className="text-left">28 57 21 87</p>
-        <p className="text-left">42 33 42 56</p>
-      </div>
+      <IonGrid>
+        <IonRow>
+          <IonCol size="4">
+            <div className="flex flex-col justify-center items-center">
+              <h3>Mail</h3>
+              <p>mhn@maincode.dk</p>
+              <p>mark@maincode.dk</p>
+            </div>
+          </IonCol>
+          <IonCol size="4">
+            <div className="w-full flex justify-center items-center">
+              <IonImg className={styles.img} src={maincodeRobot} />
+            </div>
+          </IonCol>
+          <IonCol size="4">
+            <div className="flex flex-col justify-center items-center">
+              <h3>Telefon</h3>
+              <p className="text-left">28 57 21 87</p>
+              <p className="text-left">42 33 42 56</p>
+            </div>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
     </div>
   );
 };
