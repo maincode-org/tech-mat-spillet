ca9ab7ca34e03bdc8cef809ee7ece6ad229f930b
mathias <mathias.hoeyrup@gmail.com>
2021-08-25 19:06:37 +0200

    Made character development bullets

--- ionic-app/src/components/slides/character-development/CharacterDevelopment.tsx
+++ ionic-app/src/components/slides/character-development/CharacterDevelopment.tsx
@@ -1,4 +1,5 @@
 import styles from './character-development.module.css';
+import AutocenteredBullets from '../../autocentered-bullets/AutocenteredBullets';
 /** Describe the purpose of the CharacterDevelopment component here.*/
 
 type IProps = {
@@ -6,6 +7,12 @@ type IProps = {
 };
 
 const CharacterDevelopment: React.FC<IProps> = ({ className }) => {
-  return <div className={className}></div>;
+  return (
+    <AutocenteredBullets>
+      <h3>{'>'} Din karakter følger dig med rundt på mission</h3>
+      <h3>{'>'} Jo flere opgaver klaret jo mere udstyr og medaljer optjenes</h3>
+      <h3>{'>'} Din karakter bliver bedre og giver klassen flere fælles point</h3>
+    </AutocenteredBullets>
+  );
 };
 export default CharacterDevelopment;
