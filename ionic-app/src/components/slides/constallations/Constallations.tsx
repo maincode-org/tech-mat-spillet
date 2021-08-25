e281f49ad6e7b7b9aa4e472d2f4f9fa6b0baec80
mathias <mathias.hoeyrup@gmail.com>
2021-08-25 20:18:28 +0200

    Fixed so it does not say "at Clio"

--- ionic-app/src/components/slides/constallations/Constallations.tsx
+++ ionic-app/src/components/slides/constallations/Constallations.tsx
@@ -1,4 +1,3 @@
-import styles from './constallations.module.css';
 import AutocenteredBullets from '../../autocentered-bullets/AutocenteredBullets';
 /** Describe the purpose of the Constallations component here.*/
 
@@ -10,7 +9,7 @@ const Constallations: React.FC<IProps> = ({ className }) => {
   return (
     <AutocenteredBullets>
       <h3>{'>'} Leverandøraftale</h3>
-      <h3>{'>'} Samarbejde - vi udvikler gratis - det ligger på Clio </h3>
+      <h3>{'>'} Samarbejde - vi udvikler gratis - det ligger hos jer </h3>
       <h3>{'>'} Konsulentaftale</h3>
       <h3>{'>'} Andet?</h3>
     </AutocenteredBullets>
