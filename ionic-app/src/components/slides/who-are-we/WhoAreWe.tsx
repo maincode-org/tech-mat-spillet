479c10d51c87d16b7a7e31ebb56610f7941c2413
Markido <markido@live.dk>
2021-08-25 13:11:17 +0200

    Who are we slide is done

--- ionic-app/src/components/slides/who-are-we/WhoAreWe.tsx
+++ ionic-app/src/components/slides/who-are-we/WhoAreWe.tsx
@@ -1,11 +1,18 @@
-/** Describe the purpose of the WhoAreWe component here.*/
 import styles from './who-are-we.module.css';
+import { IonImg } from '@ionic/react';
+import robot from './../../../assets/maincode-robot.png';
+import photo from './../../../assets/us-photo.jpg';
 
 type IProps = {
   className?: string;
 };
 
 const WhoAreWe: React.FC<IProps> = ({ className }) => {
-  return <div className={className}></div>;
+  return (
+    <div className={`${className} flex flex-column justify-center mt-4 ml-4 h-full`}>
+      <IonImg className={`${styles.photo} rounded box-shadow mr-3`} alt="Os" src={photo} />
+      <IonImg className={styles.robot} alt="Robot" src={robot} />
+    </div>
+  );
 };
 export default WhoAreWe;
